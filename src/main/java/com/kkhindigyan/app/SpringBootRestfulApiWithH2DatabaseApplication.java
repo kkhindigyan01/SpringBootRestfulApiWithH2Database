83fbd7db745d2aa1fd7c3bc4ac86cf794998ee10
kkhindigyan01 <kkhindigyan01@gmail.com>
2021-06-11 14:56:06 +0530

    first commit

---
+++ src/main/java/com/kkhindigyan/app/SpringBootRestfulApiWithH2DatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.kkhindigyan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulApiWithH2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulApiWithH2DatabaseApplication.class, args);
+	}
+}
